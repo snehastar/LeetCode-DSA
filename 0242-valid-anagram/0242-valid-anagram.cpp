528508298aff571204c67ed6416f1b1bf06435e6
Sneha Shukla <snehashuklastar1011@gmail.com>
2023-08-04 10:59:56 +0530

    Time: 3 ms (98.34%), Space: 7.3 MB (38.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s==t)
-            return true;
-        return false;    
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if(s==t)
+        //     return true;
+        // return false;  
+
+        // using hashmap
+        //if the count doesn't become 0 at the end means it's not an anagram
+    
+        int n = s.size();
+        if(n != t.size())
+            return false;
+        int arr[26] = {0};
+        for(int i=0; i<n; i++){
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i] != 0)
+                return false;
+        }
+        return true;
     }
 };
