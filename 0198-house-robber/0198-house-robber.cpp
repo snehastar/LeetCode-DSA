d15e35e4799e5dc51e78ea8cf4cb9af62d2a0d4a
Sneha Shukla <snehashuklastar1011@gmail.com>
2023-08-09 18:10:23 +0530

    Time: 0 ms (100%), Space: 7.5 MB (93.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -65,9 +65,7 @@ public:
         int prev = nums[0]; 
         int prev2 = 0;
         for(int i=1; i<n; i++){
-            int pick = nums[i];
-            if(i>1)
-                pick += prev2;
+            int pick = nums[i] + prev2;
             int notpick = 0 + prev;
             int curr = max(pick,notpick);
             
