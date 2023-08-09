62f71cf2c5a2f19e9536cef42aa1b899ff4b2245
Sneha Shukla <snehashuklastar1011@gmail.com>
2023-08-09 12:17:27 +0530

    Time: 0 ms (100%), Space: 7.7 MB (43.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+//RECURSION
+// tc: O(2^n) sc:O(2n)
+    //    int f(int n,vector<int>& nums){
+    //     if(n == 0)
+    //         return nums[0];
+    //     if(n < 0)
+    //         return 0;
+    //     int pick = nums[n] + f(n-2, nums);
+    //     int notpick = 0 + f(n-1, nums);
+
+    //     return max(pick, notpick);
+    // }
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     return f(n-1,nums);        
+    // } 
+
 //MEMOIZATION
+// tc: O(n) sc:O(2n)
     // int f(int i,vector<int>& nums, vector<int> &dp){
         
     //     if(i == 0)
@@ -22,20 +40,41 @@ public:
     // }
 
 //TABULATION
-    int f(int n,vector<int>& nums, vector<int> dp){
-        dp[0] = nums[0]; 
+// tc: O(n) sc:O(n)
+    // int f(int n,vector<int>& nums, vector<int> dp){
+    //     dp[0] = nums[0]; 
+    //     for(int i=1; i<n; i++){
+    //         int pick = nums[i];
+    //         if(i>1)
+    //             pick += dp[i-2];
+    //         int notpick = 0 + dp[i-1];
+    //         dp[i] = max(pick,notpick);
+    //     }
+    //     return dp[n-1];
+    // }
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> dp(n,-1); 
+    //     return f(n,nums, dp);        
+    // }
+
+//SPACE OPTIMIZATION
+// tc: O(n) sc:O(1)
+    int rob(vector<int>& nums) {
+        int n = nums.size(); 
+        int prev = nums[0]; 
+        int prev2 = 0;
         for(int i=1; i<n; i++){
             int pick = nums[i];
             if(i>1)
-                pick += dp[i-2];
-            int notpick = 0 + dp[i-1];
-            dp[i] = max(pick,notpick);
+                pick += prev2;
+            int notpick = 0 + prev;
+            int curr = max(pick,notpick);
+            
+            prev2 = prev;
+            prev = curr;
+            
         }
-        return dp[n-1];
-    }
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1); 
-        return f(n,nums, dp);        
+        return prev;
     }
 };
