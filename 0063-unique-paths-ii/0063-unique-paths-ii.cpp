94db4e5fd9b54ad9cf5961f2497deac1d4fe5bd9
Sneha Shukla <snehashuklastar1011@gmail.com>
2023-08-10 13:07:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.16%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -29,7 +29,7 @@ public:
 //DP MEMOIZATION
 //TC: O(N*M)
 //SC: O(N-1)+O(M-1) + O(N*M) //STACK SPACE AND DP ARRAY
-class Solution {
+/*class Solution {
 public:
 
     int f(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp){
@@ -54,5 +54,38 @@ public:
             return 0;
         int ans = f(m-1,n-1,obstacleGrid,dp);
         return ans;    
+    }
+};*/
+
+//DP TABULATION
+//TC: O(N*M)
+//SC: O(N*M) //DP ARRAY
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<long long>> dp(m,vector<long long>(n,0));
+        if(obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1)
+            return 0;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+
+                if(i==0 && j==0 && obstacleGrid[i][j] != 1){
+                    dp[i][j]=1;
+                    continue;
+                }
+            
+                long long l = 0;
+                long long up = 0;
+                if(i>0 && obstacleGrid[i][j] == 0)
+                    l = dp[i-1][j];
+                if(j>0 && obstacleGrid[i][j] == 0)
+                    up = dp[i][j-1];
+                dp[i][j] = l+up;
+            }
+        }
+        return dp[m-1][n-1];    
     }
 };
