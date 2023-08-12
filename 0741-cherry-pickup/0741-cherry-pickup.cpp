849a074a17d96c20c3e21eb548c1493dc1aaecf9
Sneha Shukla <snehashuklastar1011@gmail.com>
2023-08-12 13:16:22 +0530

    Time: 481 ms (19.66%), Space: 557.5 MB (10.23%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,6 +1,6 @@
 //RECURSIVE
-//TC: O(N^4)
-//SC: stack space
+//TC: O(2^n *2^n)
+//SC: O(n) auxiliary stack space
 // class Solution {
 // public:
    
@@ -35,12 +35,59 @@
 // };
 
 //MEMOIZATION
-//TC: O(N^4)
-//SC: O(N^4)
+//TC: O(N*N*N*N)
+//SC: O(N*N*N*N) +O(N)
+// class Solution {
+// public:
+  
+//     int f(int i1, int j1,int i2, int j2, int n, vector<vector<int>>& grid, vector<vector<vector<vector<int>>>> &dp){
+//         if(i1==n && j1==n){//base case
+//             return grid[i1][j1];//count only once
+//         }
+//         int cherries = 0;
+//         if(i1>n || j1>n || i2>n || j2>n || grid[i1][j1] == -1 || grid[i2][j2] == -1)
+//             return -1e8;
+
+//         if(dp[i1][j1][i2][j2] != -2)
+//             return dp[i1][j1][i2][j2];
+//         if(i1==i2 && j1==j2){
+//             cherries+=grid[i1][j1];
+//         }
+//         else{
+//             cherries+=grid[i1][j1]+grid[i2][j2];
+//         }
+//         int dd = f(i1+1,j1,i2+1,j2,n,grid,dp);
+//         int dr = f(i1+1,j1,i2,j2+1,n,grid,dp);
+//         int rd = f(i1,j1+1,i2+1,j2,n,grid,dp);
+//         int rr = f(i1,j1+1,i2,j2+1,n,grid,dp);
+//         int maxi = max(max(dd,dr),max(rd,rr));
+//         return dp[i1][j1][i2][j2] = maxi+cherries;
+//     }
+
+//     int cherryPickup(vector<vector<int>>& grid) {
+//         int n = grid.size();
+//         if(grid[0][0] == -1 || grid[n-1][n-1] == -2)
+//             return 0;
+//         int maxi = 0; 
+        
+//         vector<vector<vector<vector<int>>>> dp(n,
+//         vector<vector<vector<int>>> (n,
+//         vector<vector<int>> (n,
+//         vector<int>(n,-2))));   
+//         return max(maxi,f(0,0,0,0,n-1,grid,dp));
+//     }
+// };
+
+// check out favourite leetcode solution for understanding also striver's video on 3d dp
+
+//MEMOIZATION with space optimization
+//TC: O(N*N*N)
+//SC: O(N*N*N) +O(N)
 class Solution {
 public:
   
-    int f(int i1, int j1,int i2, int j2, int n, vector<vector<int>>& grid, vector<vector<vector<vector<int>>>> &dp){
+    int f(int i1, int j1,int i2, int n, vector<vector<int>>& grid, vector<vector<vector<vector<int>>>> &dp){
+        int j2 = i1+j1-i2;
         if(i1==n && j1==n){//base case
             return grid[i1][j1];//count only once
         }
@@ -56,10 +103,10 @@ public:
         else{
             cherries+=grid[i1][j1]+grid[i2][j2];
         }
-        int dd = f(i1+1,j1,i2+1,j2,n,grid,dp);
-        int dr = f(i1+1,j1,i2,j2+1,n,grid,dp);
-        int rd = f(i1,j1+1,i2+1,j2,n,grid,dp);
-        int rr = f(i1,j1+1,i2,j2+1,n,grid,dp);
+        int dd = f(i1+1,j1,i2+1,n,grid,dp);
+        int dr = f(i1+1,j1,i2,n,grid,dp);
+        int rd = f(i1,j1+1,i2+1,n,grid,dp);
+        int rr = f(i1,j1+1,i2,n,grid,dp);
         int maxi = max(max(dd,dr),max(rd,rr));
         return dp[i1][j1][i2][j2] = maxi+cherries;
     }
@@ -74,6 +121,6 @@ public:
         vector<vector<vector<int>>> (n,
         vector<vector<int>> (n,
         vector<int>(n,-2))));   
-        return max(maxi,f(0,0,0,0,n-1,grid,dp));
+        return max(maxi,f(0,0,0,n-1,grid,dp));
     }
 };
