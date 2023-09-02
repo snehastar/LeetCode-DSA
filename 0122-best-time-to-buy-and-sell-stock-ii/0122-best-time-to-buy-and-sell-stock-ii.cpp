9f89be21c80bf219ce1ab1d916d2cb83c253f731
Sneha Shukla <snehashuklastar1011@gmail.com>
2023-09-02 10:07:06 +0530

    Time: 11 ms (32.17%), Space: 15.2 MB (31.31%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,43 @@
+//RECURSIVE -> MEMOIZATION
+// int f(int i, int status, vector<int>& prices, vector<vector<int>> &dp){
+//         if(i == prices.size())
+//             return 0;
+            
+//         int profit = 0;
+
+//         if(dp[i][status] != -1)
+//             return dp[i][status];
+
+//         if(status == 1) //buy
+//             profit =  max(-prices[i] + f(i+1,0,prices,dp), f(i+1,1,prices,dp));
+//         else if(status == 0) //sell
+//             profit = max(prices[i] + f(i+1,1,prices,dp), f(i+1,0,prices,dp));
+//         return dp[i][status] = profit;
+//     }
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         if(n<=1)
+//             return 0;
+//         vector<vector<int>> dp(n,vector<int>(n,-1));
+//         return f(0,1,prices,dp);        
+//     }
+class Solution {
+public:
+//TABULATION
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        //reverse directed od memoization
+        for(int i=n-1; i>=0; i--){
+            for(int status=0; status<=1; status++){
+                int profit = 0;
+                if(status == 1) //buy
+                    profit =  max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+                else if(status == 0) //sell
+                    profit = max(prices[i] + dp[i+1][1], dp[i+1][0]);
+                dp[i][status] = profit;   
+            }
+        }
+        return dp[0][1];       
+    }
+};
